95e272d69c6a8ecea65e60761a699019e073625e
Chris Liu <guohong.liu@gmail.com>
2016-05-29 19:49:20 +0800

    using pmap to utilize multi-core to speed up benchmark

--- bench.py
+++ bench.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import subprocess
+from multiprocessing import Pool
 
 def wins(summary):
     lines = summary.split('\n')
@@ -8,12 +9,12 @@ def wins(summary):
         if line.startswith('won:'):
             return int(line.split()[1])
 
-def bench():
-    score = 0
+def level_score(level):
     weights = [1,2,4]
-    for level in range(1,4):
-        s = weights[level-1]*wins(subprocess.check_output(["./mine-" + str(level)]))
-        score += s
-    return score
+    return weights[level-1]*wins(subprocess.check_output(["./mine-" + str(level)]))
+
+def bench():
+    p = Pool(4)
+    return sum(p.map(level_score, range(1, 4)))
 
 print bench()
