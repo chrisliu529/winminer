47ac231eca24e7e27ac402a3d9eb9a122d891d26
Chris Liu <guohong.liu@gmail.com>
2016-10-19 10:07:24 +0800

    10x benchmark test cases

--- gen_cases.sh
+++ gen_cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./winminer -gb -lv 1 -s 1 -n 1000 > cases.txt
-./winminer -gb -lv 2 -s 2 -n 500 >> cases.txt
-./winminer -gb -lv 3 -s 3 -n 250 >> cases.txt
+./winminer -gb -lv 1 -s 1 -n 10000 > cases.txt
+./winminer -gb -lv 2 -s 2 -n 5000 >> cases.txt
+./winminer -gb -lv 3 -s 3 -n 2500 >> cases.txt
 
